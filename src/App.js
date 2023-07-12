6155af0ce0bc80b7d7a2b862c61681f3087fadb4
unknown <smeena1@es.iitr.ac.in>
2023-07-12 06:05:37 +0530

    props practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+const[data,setData] = useState(0);
+    function funk()
+    {
+        for(let i=1;i<5;i++){
+            console.log(setData(data+1));
+        }
+    }
+
+    return(
+        <>
+        <button onClick={funk}>myButton</button>
+        </>
+    )
 }
 
 export default App;
---
+++ src/Student.js
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+function Student(Pros){
+    // console.log(Pros.name);
+    return(
+        <>
+         <h1>This student is from other component {Pros.Name}</h1>
+        </>
+    )
+}
+
+export default Student;
