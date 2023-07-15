60e88fd3b01a9c114578cb9d5ec2d663a22aea06
unknown <smeena1@es.iitr.ac.in>
2023-07-15 19:50:50 +0530

    get val

--- src/Adding_page.js
+++ src/Adding_page.js
@@ -3,6 +3,7 @@ import react ,{ useState } from 'react'
 
 function Adding_page() {
 const [data_n , change_val] = useState("Rajjo");
+const [data_ch, getVal] = useState("");
 
 const callFunction = ()=>{
     change_val("Rajkuwar");
@@ -12,11 +13,18 @@ const reVert = ()=> {
 change_val("Rajjo");
 }
 
+const valueGet =(e)=>{
+    getVal(e.target.value);
+}
   return (
     <div>
         <h1>This Third page {data_n}</h1>
         <button onClick={callFunction}>Full name</button>
         <button onClick={reVert}>Revert Change</button>
+        <br />
+        <p>Now get the Value of Input box vale {data_ch}</p>
+        <input type="text" onChange={valueGet} />
+
     </div>
   )
 }
